eef8c6ca5e3e861ba68b8a49b19c3722569a0110
magda <stenius.mimmy@gmail.com>
2021-01-18 16:10:22 +0200

    Add CLI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import CLI from './CLI';
 import Safety from './Safety';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
-  return <Safety />
+  return(
+  <Router>
+    <div style={{margin: '.2em 25em'}}>
+      <nav style={{display: 'flex', justifyContent: 'space-between'}}>
+        <Link to="/">Home</Link>
+        <Link to="/notes">Notes</Link>
+        <Link to="/safety">Just the CV</Link>
+      </nav>
+      <Switch>
+        <Route path="/safety">
+          <Safety />
+        </Route>
+        <Route path="/notes">
+          <p>Notes will be arriving shortly...</p>
+        </Route>
+        <Route path="/">
+          <CLI />
+        </Route>
+      </Switch>
+    </div>
+  </Router>)
 }
 
 export default App;
---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+    blue: '#002E96',
+    lila: "#80198c",
+    darkPink: "#ba0072",
+    red: "#db2950",
+    orange: "#e55d2c",
+    yellow: "#db8d00",
+  }
